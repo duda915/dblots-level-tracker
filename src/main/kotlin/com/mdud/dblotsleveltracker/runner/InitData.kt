ccaadc1bd8d830b058d1b092fce3b73c65e2ee48
MDud <duda915@gmail.com>
2018-09-12 20:52:11 +0200

    JSoup dependency, parse function template inside InitData

--- src/main/kotlin/com/mdud/dblotsleveltracker/runner/InitData.kt
+++ src/main/kotlin/com/mdud/dblotsleveltracker/runner/InitData.kt
@@ -4,11 +4,14 @@ import com.mdud.dblotsleveltracker.model.Advance
 import com.mdud.dblotsleveltracker.model.Character
 import com.mdud.dblotsleveltracker.model.Race
 import com.mdud.dblotsleveltracker.repository.CharacterRepository
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
 import java.sql.Date
-import java.time.ZoneId
 import java.util.*
 
 @Component
@@ -31,6 +34,19 @@ class InitData (@Autowired val characterRepository: CharacterRepository) : Comma
         }
 
         characterRepository.save(Character(null, "Test Multiple", Race.GokuBlack, advanceList))
+
+        // parse who is online
+        val doc : Document = Jsoup.connect("http://dblots.org.pl/whoisonline.php?lang=en&s=classic").get()
+        val elements : Elements = doc.select(".mid table tr")
+        for(row in elements) {
+            val cells = row.select("td")
+            val id = cells.getOrNull(0)?.html()
+            val raceImageUrl = cells.getOrNull(1)?.select("img")?.attr("src")
+            val name = cells.getOrNull(2)?.select("a")?.html()
+            val level = cells.getOrNull(3)?.html()
+
+            println("$id | $raceImageUrl | $name | $level")
+        }
     }
 
 }
